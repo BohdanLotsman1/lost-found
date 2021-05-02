7e285130b66de457d7053507b55456405155c534
BohdanLotsman1 <boglotsman@gmail.com>
2021-05-02 22:21:54 +0300

    demo version frontend

--- src/modules/User/components/Profiles/AdminProfile/AdminProfile.tsx
+++ src/modules/User/components/Profiles/AdminProfile/AdminProfile.tsx
@@ -2,12 +2,14 @@ import  React, {useEffect} from 'react';
 import {Tabs} from 'antd';
 import 'antd/dist/antd.css';
 import './style.scss';
-import { useDispatch } from 'react-redux';
-import { getPosts } from '../../../../Posts/store/actions';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPosts, getUsersPosts } from '../../../../Posts/store/actions';
 import UserList from './Tabs/UserList/UserList';
 import PostsList from './Tabs/PostsList/PostsList';
 import { fetchAllUsers } from '../../../store/actions';
 import PersonalInfo from '../../PersonalInfo/PersonalInfo';
+import CustomerPostsList from '../CustomerProfile/CustomerPostsList/CustomerPostsList';
+import { getUserSelector } from '../../../store/selectors';
 
 
 const { TabPane } = Tabs;
@@ -15,10 +17,12 @@ const { TabPane } = Tabs;
 const Admin = () => {
   
   const dispatch = useDispatch();
-
+  const user = useSelector(getUserSelector);
+  
   useEffect(()=>{
     dispatch(fetchAllUsers(1))
     dispatch(getPosts(1))
+    dispatch(getUsersPosts(user.id))
   },[])
 
   return(
@@ -30,6 +34,9 @@ const Admin = () => {
         </TabPane>
         <TabPane tab="Posts" key="2">
           <PostsList/>
+        </TabPane>
+        <TabPane tab="My posts" key="3">
+          <CustomerPostsList/>
         </TabPane>
         <TabPane tab="Personal info" key="4">
           <PersonalInfo/>
